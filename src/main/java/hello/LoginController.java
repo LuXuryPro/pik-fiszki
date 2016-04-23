a423451d5a071ae0da56e6930d75908db8daaa4e
Adrian Stachlewski <adrian.stachlewski@gmail.com>
2016-04-23 21:41:12 +0200

    Proporties cleaning

--- src/main/java/hello/LoginController.java
+++ src/main/java/hello/LoginController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController
 {
-        @RequestMapping("/login")
+        @RequestMapping("/login2")
         public String login()
         {
             return "login";
