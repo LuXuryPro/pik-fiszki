3c344727e3e138ae8996ce007d5924a07d68d698
Michał Kamiński <M.J.Kaminski@stud.elka.pw.edu.pl>
2016-04-23 23:14:48 +0200

    Obsługa MongoDB

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,16 +1,27 @@
 package hello;
 
+import dto.UserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import repositories.UserRepository;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class GreetingController {
 
+
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
+
         return "greeting-test";
     }
 
@@ -19,4 +30,6 @@ public class GreetingController {
         model.addAttribute("name", name);
         return "greeting-test";
     }
+
+
 }
