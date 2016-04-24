a8841bb05903336d405c716725ef5d6d42cd83d9
Radek <radek1236987@gmail.com>
2016-04-24 10:51:10 +0200

    Pobieranie info za facebook

--- src/main/java/hello/FacebookController.java
+++ src/main/java/hello/FacebookController.java
@@ -3,6 +3,14 @@ package hello;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.token.Token;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.impl.FacebookTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +19,15 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 @EnableOAuth2Sso
-@RestController
+@Controller
 public class FacebookController extends WebSecurityConfigurerAdapter {
     @RequestMapping("/user")
-    public Map<String, String> user(Principal principal) {
-        Map<String, String> map = new LinkedHashMap<>();
-        map.put("name", principal.getName());
-        return map;
+    public String user(Principal principal, Model model) {
+        OAuth2Authentication u = (OAuth2Authentication)principal;
+        OAuth2AuthenticationDetails d = (OAuth2AuthenticationDetails)u.getDetails();
+        Facebook f = new FacebookTemplate(d.getTokenValue());
+        model.addAttribute("facebookProfile", f.userOperations().getUserProfile());
+        return "user";
     }
 
     @Override
