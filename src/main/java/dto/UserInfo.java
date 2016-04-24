32138801ba52641be20f87acb736c91fc08c80db
Michał Kamiński <M.J.Kaminski@stud.elka.pw.edu.pl>
2016-04-24 16:01:49 +0200

    Próbny zapis do bazy, dodanie ID

--- src/main/java/dto/UserInfo.java
+++ src/main/java/dto/UserInfo.java
@@ -9,8 +9,11 @@ import org.springframework.data.annotation.Id;
 */
 
 @ToString
+@Getter
+@Setter
 public class UserInfo {
     @Id
+    private String userId;
     private String email;
     private String firstName;
     private String lastName;
@@ -18,33 +21,11 @@ public class UserInfo {
     public UserInfo() {
     }
 
-    public UserInfo(String firstName, String lastName, String email) {
+    public UserInfo(String id,String firstName, String lastName, String email) {
+        this.userId = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 }
