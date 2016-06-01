deb34d10ef377e6081fa88cd451aaa318f3aa8f8
Michał Kamiński <M.J.Kaminski@stud.elka.pw.edu.pl>
2016-06-01 22:50:06 +0200

    DTO: Course, User, Question

--- src/main/java/dto/QuestionInfo.java
+++ src/main/java/dto/QuestionInfo.java
@@ -1,7 +1,39 @@
 package dto;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
 /**
- * Created by Michał on 01.06.2016.
+ * Created by Michał on 01.06.2016
  */
+
+@Document(collection="question")
+@Data
 public class QuestionInfo {
+
+    @Id
+    private long id;
+
+    private String question;
+
+    private String answer;
+
+    public QuestionInfo() {
+    }
+
+    @PersistenceConstructor
+    public QuestionInfo(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    @Override
+    public String toString() {
+        return "Question: "+question+"Answer: "+answer;
+    }
+
 }
+
--- src/main/java/dto/UserInfo.java
+++ src/main/java/dto/UserInfo.java
@@ -2,18 +2,31 @@ package dto;
 
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.ArrayList;
+import java.util.List;
 
 @EqualsAndHashCode
 @Data
+@Document(collection = "user")
 public class UserInfo {
     @Id
     private String userId;
+
     private String email;
+
     private String firstName;
+
     private String lastName;
+
+    @DBRef(db="course")
+    private  List<CourseInfo> courses = new ArrayList<>();
 
     public UserInfo() {}
 
+    @PersistenceConstructor
     public UserInfo(String id,String firstName, String lastName, String email) {
         this.userId = id;
         this.firstName = firstName;
