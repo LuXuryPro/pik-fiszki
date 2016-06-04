2eb51e4629856b847beb4fd5bbc9a57f8fff29ec
Radek <radek1236987@gmail.com>
2016-06-04 18:28:33 +0200

    Add name in index page

--- src/main/java/pik/controllers/FacebookController.java
+++ src/main/java/pik/controllers/FacebookController.java
@@ -19,45 +19,11 @@ public class FacebookController {
     UserRepository users;
 
     FacebookController() {
-
     }
 
     @Autowired
     FacebookController(UserRepository users)
     {
         this.users = users;
-    }
-
-    @RequestMapping("/user")
-    public String user(Principal principal, Model model) {
-        getUserInfo(principal, model);
-        return "user";
-    }
-    @RequestMapping("/")
-    public String index(Principal principal, Model model) {
-        if (principal == null)
-            return "LoginPage";
-        else {
-            getUserInfo(principal, model);
-            return "user";
-        }
-    }
-
-    protected void getUserInfo(Principal principal, Model model) {
-        OAuth2Authentication u = (OAuth2Authentication)principal;
-        OAuth2AuthenticationDetails d = (OAuth2AuthenticationDetails)u.getDetails();
-        FacebookTemplate f = new FacebookTemplate(d.getTokenValue());
-        User usr = f.userOperations().getUserProfile();
-
-        UserInfo userData;
-        if(!users.exists(usr.getId())){
-            userData = new UserInfo(usr.getId(),usr.getFirstName(),usr.getLastName(),usr.getEmail());
-            users.save(userData);
-        }
-        else
-            userData = users.findByuserId(usr.getId());
-
-        model.addAttribute("facebookProfile", f.userOperations().getUserProfile());
-        model.addAttribute("dataBaseProfile", userData);
     }
 }
