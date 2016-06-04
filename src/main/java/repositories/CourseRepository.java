c2f1c49fa166920b6bdcbc0b768a2ac57998237d
Michał Kamiński <M.J.Kaminski@stud.elka.pw.edu.pl>
2016-06-04 14:22:49 +0200

    Repozytoria i serwisy - nowa nadzieja

---
+++ src/main/java/repositories/CourseRepository.java
@@ -0,0 +1,18 @@
+package repositories;
+
+import dto.CourseInfo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.math.BigInteger;
+import java.util.List;
+
+/**
+ * Created by Michał on 03.06.2016.
+ */
+
+public interface CourseRepository extends MongoRepository<CourseInfo,BigInteger> {
+    CourseInfo findById(BigInteger id);
+    CourseInfo findByName(String name);
+    List<CourseInfo> findByOwnerId(String ownerId);
+
+}
---
+++ src/main/java/services/UserService.java
@@ -0,0 +1,71 @@
+package services;
+
+import java.util.List;
+import java.util.UUID;
+;
+import dto.UserInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import repositories.MarkRepository;
+import repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+    @Autowired
+    private MarkRepository markRepository;
+
+
+	public UserInfo create(UserInfo user) {
+		return userRepository.save(user);
+	}
+
+	public UserInfo read(UserInfo user) {
+		return user;
+	}
+
+    public List<UserInfo> readAll() {
+        return userRepository.findAll();
+    }
+
+	public Page<UserInfo> readAll(Pageable pageable) {
+        return userRepository.findAll(pageable);
+	}
+
+	/*public UserInfo update(UserInfo user) {
+		UserInfo existingUser = userRepository.findByUsername(user.getUsername());
+
+		if (existingUser == null) {
+			return null;
+		}
+
+		existingUser.setFirstName(user.getFirstName());
+		existingUser.setLastName(user.getLastName());
+		existingUser.getRole().setRole(user.getRole().getRole());
+
+		// We must save both separately since there is no cascading feature
+		// in Spring Data MongoDB (for now)
+		roleRepository.save(existingUser.getRole());
+		return userRepository.save(existingUser);
+	}
+
+	public Boolean delete(User user) {
+		User existingUser = userRepository.findByUsername(user.getUsername());
+
+		if (existingUser == null) {
+			return false;
+		}
+
+		// We must delete both separately since there is no cascading feature
+		// in Spring Data MongoDB (for now)
+		roleRepository.delete(existingUser.getRole());
+		userRepository.delete(existingUser);
+		return true;
+	}*/
+}
+
