1f8ed4bfabe293f190d14fdda5bbbea6c1ca36ca
Radek <radek@rzaluska.com>
2016-06-04 14:08:18 +0200

    Add classic getters and setters to UserInfo
    Lombok is problematic so use normal getters and setters

--- src/main/java/pik/dto/UserInfo.java
+++ src/main/java/pik/dto/UserInfo.java
@@ -3,13 +3,45 @@ package pik.dto;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 
-@EqualsAndHashCode
 @Data
 public class UserInfo {
     @Id
     private String userId;
     private String email;
     private String firstName;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     private String lastName;
 
     public UserInfo() {}
