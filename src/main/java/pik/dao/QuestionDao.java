4971094000eb1f1de30e040fea7b43f3cd42d293
Michał Kamiński <M.J.Kaminski@stud.elka.pw.edu.pl>
2016-06-04 21:36:24 +0200

    Question Dao

---
+++ src/main/java/pik/dao/QuestionDao.java
@@ -0,0 +1,22 @@
+package pik.dao;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import pik.dto.CourseInfo;
+import pik.dto.QuestionInfo;
+import pik.dto.UserInfo;
+
+import java.math.BigInteger;
+import java.util.List;
+
+/**
+ * Created by Michał on 04.06.2016.
+ */
+public interface QuestionDao {
+    QuestionInfo add(QuestionInfo questionInfo);
+    QuestionInfo update(QuestionInfo questionInfo);
+    QuestionInfo remove(QuestionInfo questionInfo);
+    List<QuestionInfo> getCourseQuestions(BigInteger courseId);
+    Page<QuestionInfo> getQuestionToAnswer(UserInfo user, CourseInfo course, Pageable page);
+
+}
