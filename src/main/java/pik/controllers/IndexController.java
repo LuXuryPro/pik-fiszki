7a714260d40afa2a0291bded6f55d259c2380e1b
Radek <radek1236987@gmail.com>
2016-06-04 18:48:11 +0200

    Add logout function

--- src/main/java/pik/controllers/IndexController.java
+++ src/main/java/pik/controllers/IndexController.java
@@ -1,5 +1,9 @@
 package pik.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.web.bind.annotation.RequestMethod;
 import pik.Util.FacebookHelper;
 import pik.dto.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -65,6 +69,15 @@ public class IndexController {
         else {
             return "redirect:index";
         }
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/";
     }
 }
 
