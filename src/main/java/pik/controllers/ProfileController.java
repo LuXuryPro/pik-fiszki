5adf24561ce9cda95c0cf50d9fd6da75bd761480
Radek <radek@rzaluska.com>
2016-06-04 23:54:44 +0200

    Add ability to edit user name in preferences

--- src/main/java/pik/controllers/ProfileController.java
+++ src/main/java/pik/controllers/ProfileController.java
@@ -4,7 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.facebook.api.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pik.JSONDao.EditProfileJsonRequest;
+import pik.JSONDao.EditProfileJsonResponse;
 import pik.Util.FacebookHelper;
 import pik.dto.UserInfo;
 import pik.repositories.UserRepository;
@@ -14,10 +19,12 @@ import java.security.Principal;
 @Controller
 public class ProfileController {
     private UserRepository userRepository;
+
     @Autowired
     ProfileController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
     @RequestMapping("/me")
     public String showProfile(Principal principal, Model model) {
         FacebookHelper f = new FacebookHelper(principal);
@@ -25,11 +32,26 @@ public class ProfileController {
         model.addAttribute("user", user);
         return "me";
     }
+
     @RequestMapping("/preferences")
-    public String showProfile(Principal principal, Model model) {
+    public String editProfile(Principal principal, Model model) {
         FacebookHelper f = new FacebookHelper(principal);
         UserInfo user = this.userRepository.findByuserId(f.getId());
         model.addAttribute("user", user);
         return "preferences";
+    }
+    @RequestMapping(value = "/saveUserForm")
+    @ResponseBody
+    public EditProfileJsonResponse addPerson(@RequestBody EditProfileJsonRequest editProfileJsonRequest) {
+        String name = editProfileJsonRequest.getFirstName();
+        String surname = editProfileJsonRequest.getSecondName();
+        String email = editProfileJsonRequest.getEmail();
+        String id = editProfileJsonRequest.getId();
+        UserInfo userInfo = this.userRepository.findByuserId(id);
+        userInfo.setFirstName(name);
+        userInfo.setLastName(surname);
+        userInfo.setEmail(email);
+        this.userRepository.save(userInfo);
+        return new EditProfileJsonResponse("OK");
     }
 }
