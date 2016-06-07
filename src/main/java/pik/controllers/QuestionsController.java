65c21c6b76fd72ed74dcd23fe09a874a506abbb3
Adrian Stachlewski <adrian.stachlewski@gmail.com>
2016-06-08 01:08:37 +0200

    Komentarze cz.3

--- src/main/java/pik/controllers/QuestionsController.java
+++ src/main/java/pik/controllers/QuestionsController.java
@@ -27,6 +27,13 @@ public class QuestionsController {
     private CourseDao courseDao;
     private QuestionDao questionDao;
 
+    /**
+     * Konstruktor kontrolera pytań
+     *
+     * @param userDao     DAO użytkowników
+     * @param courseDao   DAO kursów
+     * @param questionDao DAO pytań
+     */
     @Autowired
     QuestionsController(UserDao userDao, CourseDao courseDao, QuestionDao questionDao) {
         this.userDao = userDao;
@@ -34,10 +41,24 @@ public class QuestionsController {
         this.questionDao = questionDao;
     }
 
+    /**
+     * Funkcja zwracająca liczbę pytań dla danego kursu
+     *
+     * @param courseId ID kursu
+     * @return Liczba pytań w kursie.
+     */
     public Long countAllQuestions(BigInteger courseId) {
         return questionDao.countQuestions(courseId);
     }
 
+    /**
+     * Funkcja zliczająca liczbę pytań w danym kursie, które oczekują na użytkownika.
+     *
+     * @param userId   ID użytkownika
+     * @param courseId ID kursu
+     * @return Liczba gotowych pytań.
+     * @throws CourseAccessException Próba dostępu do nieuprawnionego kursu.
+     */
     public Integer countActiveQuestions(String userId, BigInteger courseId) throws CourseAccessException {
         UserInfo userInfo = userDao.getById(userId);
         if (userInfo.getSubscribedCourses().contains(courseId)) {
@@ -48,6 +69,14 @@ public class QuestionsController {
         }
     }
 
+    /**
+     * Pobierz pierwsze pytanie do wyświetlenia
+     *
+     * @param userId   ID użytkownika
+     * @param courseId ID kursów
+     * @return Pierwsze pytanie w kolejce
+     * @throws CourseAccessException Próba dostępu do kursu, na który użytkownik nie jest zapisany.
+     */
     public QuestionInfo getFirstQuestion(String userId, BigInteger courseId) throws CourseAccessException {
         UserInfo userInfo = userDao.getById(userId);
         CourseInfo courseInfo = courseDao.get(courseId);
@@ -60,6 +89,14 @@ public class QuestionsController {
         }
     }
 
+    /**
+     * Pobierz wszystkie pytania.
+     *
+     * @param userId   ID usera
+     * @param courseId ID kursu
+     * @return Lista pytań
+     * @throws CourseAccessException Próba dostępu do kursu, na który dana osoba nie jest zapisana.
+     */
     public List<QuestionInfo> getAllQuestions(String userId, BigInteger courseId) throws CourseAccessException {
         UserInfo userInfo = userDao.getById(userId);
         if (userInfo.getSubscribedCourses().contains(courseId)) {
@@ -70,6 +107,16 @@ public class QuestionsController {
         }
     }
 
+    /**
+     * Funkcja dodająca pytanie do bazy danych w danym kursie.
+     *
+     * @param answer   pytanie
+     * @param question odpowiedź
+     * @param courseId ID kursu
+     * @param userId   ID użytkownika
+     * @return Informacja o powodzeniu zadania
+     * @throws CourseAccessException Próba dostępu do kursu, którego nie jest się właścicielem.
+     */
     public Boolean addQuestion(String answer, String question, BigInteger courseId, String userId)
             throws CourseAccessException {
         CourseInfo courseInfo = courseDao.get(courseId);
@@ -84,6 +131,14 @@ public class QuestionsController {
         return questionDao.add(questionInfo) != null;
     }
 
+    /**
+     * Edycja pytania w bazie danych
+     *
+     * @param questionInfo DTO z pytaniem
+     * @param userId       ID użytkownika
+     * @return Informacja o powodzeniu operacji.
+     * @throws CourseAccessException Informajca o próbie dostępu do nieswojego kursu.
+     */
     public Boolean editQuestion(QuestionInfo questionInfo, String userId)
             throws CourseAccessException {
         BigInteger courseId = questionInfo.getCourseId();
@@ -94,6 +149,14 @@ public class QuestionsController {
         return questionDao.update(questionInfo) != null;
     }
 
+    /**
+     * Usunięcie pytania z kursu.
+     *
+     * @param questionInfo DTO z pytaniem do usunięcia.
+     * @param userId       ID użytkownika
+     * @return Informacja o powodzeniu operacji.
+     * @throws CourseAccessException Próba dostępu do nieswojego kursu.
+     */
     public Boolean deleteQuestion(QuestionInfo questionInfo, String userId) throws CourseAccessException {
         BigInteger courseId = questionInfo.getCourseId();
         CourseInfo courseInfo = courseDao.get(courseId);
@@ -103,12 +166,19 @@ public class QuestionsController {
         return questionDao.remove(questionInfo);
     }
 
+    /**
+     * Funkcja służąca do ocenienia pytania.
+     *
+     * @param questionInfo DTO z pytaniem (wystaczy podanie id pytania oraz kursu)
+     * @param userId       ID użytkownika
+     * @param score        ocena
+     * @return Informacja o powodzeniu operacji.
+     */
     public Boolean markQuestion(QuestionInfo questionInfo, String userId, int score) {
         UserInfo userInfo = userDao.getById(userId);
         List<MarkInfo> markInfoList = userInfo.getMarks();
         BigInteger questionId = questionInfo.getId();
         BigInteger courseId = questionInfo.getCourseId();
-        MarkInfo mark;
         for (MarkInfo markInfo : markInfoList) {
             if (markInfo.getCourseId().equals(courseId) && markInfo.getQuestionId().equals(questionId)) {
                 markAlgorithm(markInfo, score);
@@ -118,6 +188,12 @@ public class QuestionsController {
         return userDao.update(userInfo) != null;
     }
 
+    /**
+     * Funkcja wyliczająca nowe wartości parametrów algorytmu SuperMemo.
+     *
+     * @param markInfoOld DTO z paramaterami przed ocenami. Nastawia wartości na nowe.
+     * @param score Ocena
+     */
     private void markAlgorithm(MarkInfo markInfoOld, int score) {
         markInfoOld.setCounter(markInfoOld.getCounter() + 1);
         int counter = markInfoOld.getCounter();
