14d6004b36743b4d68d516a5bb14896c9ab21bee
Konrad <k.czarnota27@gmail.com>
2016-06-07 20:18:26 +0200

    Show courses

--- src/main/java/pik/controllers/CourseController.java
+++ src/main/java/pik/controllers/CourseController.java
@@ -3,6 +3,8 @@ package pik.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 import pik.dao.CourseDao;
 import pik.dao.UserDao;
 import pik.dto.CourseInfo;
