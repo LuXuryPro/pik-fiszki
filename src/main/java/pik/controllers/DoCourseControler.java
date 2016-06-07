efb4a72639a6ee0a122027eba4c0de21d1313435
Radek <radek@rzaluska.com>
2016-06-08 01:02:50 +0200

    Add do-course URL

--- src/main/java/pik/controllers/DoCourseControler.java
+++ src/main/java/pik/controllers/DoCourseControler.java
@@ -2,6 +2,8 @@ package pik.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,6 +24,14 @@ public class DoCourseControler {
     @Autowired
     public DoCourseControler(QuestionsController questionsController) {
         this.questionsController = questionsController;
+    }
+
+    @RequestMapping("/do-course/{courseID}")
+    public String doCourse(@PathVariable(value="courseID") String courseId, Principal principal, Model model) {
+        FacebookHelper facebookHelper = new FacebookHelper(principal);
+        model.addAttribute("userId", facebookHelper.getId());
+        model.addAttribute("courseId", courseId);
+        return "do-course";
     }
 
     @RequestMapping("answer")
