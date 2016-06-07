3b57a7e971df009a15827a7ff6350b15d1de9008
Konrad <k.czarnota27@gmail.com>
2016-06-07 19:55:34 +0200

    Merge branch 'master' of /home/konrad/Dokumenty/IdeaProjects/pik-fiszki with conflicts.

---
+++ src/main/java/pik/controllers/CourseController.java
@@ -0,0 +1,41 @@
+package pik.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pik.dao.CourseDao;
+import pik.dao.UserDao;
+import pik.dto.CourseInfo;
+
+import java.security.Principal;
+import java.util.List;
+
+/**
+ * Created by adrian on 07.06.16.
+ */
+
+@Controller
+public class CourseController {
+    // dodaj, edytowac, usunac, pobrac rozne wersje
+    private UserDao userDao;
+    private CourseDao courseDao;
+
+    @Autowired
+    public CourseController(UserDao userDao, CourseDao courseDao) {
+        this.userDao = userDao;
+        this.courseDao = courseDao;
+    }
+
+    public Boolean addCourse(String name, String description, String ownerId) {
+        CourseInfo courseInfo = new CourseInfo();
+        courseInfo.setName(name);
+        courseInfo.setDescription(description);
+        courseInfo.setOwnerId(ownerId);
+        return courseDao.create(courseInfo) != null;
+    }
+
+    public List<CourseInfo> getSubscribedCourses(String userId) {
+        return courseDao.getSubscribedCourses(userDao.getById(userId));
+    }
+}
