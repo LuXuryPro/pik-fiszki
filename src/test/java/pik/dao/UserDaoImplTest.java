85b464ca030c1bd583f2b9cb56fec5da2883eeb3
Michał Kamiński <M.J.Kaminski@stud.elka.pw.edu.pl>
2016-06-07 22:45:04 +0200

    Testy UserDaoImpl

--- src/test/java/pik/dao/UserDaoImplTest.java
+++ src/test/java/pik/dao/UserDaoImplTest.java
@@ -1,59 +1,125 @@
 package pik.dao;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.springframework.data.mongodb.core.MongoOperations;
+import pik.dto.CourseInfo;
+import pik.dto.MarkInfo;
+import pik.dto.QuestionInfo;
+import pik.dto.UserInfo;
+import pik.repositories.CourseRepository;
+import pik.repositories.MarkRepository;
+import pik.repositories.QuestionRepository;
+import pik.repositories.UserRepository;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.Mockito.when;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 
-/**
- * Created by Michał on 07.06.2016.
- */
+
 @RunWith(PowerMockRunner.class)
 public class UserDaoImplTest {
-    @Test
-    public void create() throws Exception {
+
+    @Mock
+    private QuestionRepository mockQuestionRep;
+
+    @Mock
+    private UserRepository mockUserRep;
+
+    @Mock
+    private MarkRepository mockMarkrep;
+
+    @Mock
+    private CourseRepository mockCourseRep;
+
+    private UserDaoImpl dao;
+
+
+    @Before
+    public void setUp(){
+        when(mockUserRep.findByUserId(anyString())).thenAnswer(invocation -> {
+            UserInfo user = new UserInfo();
+            user.setUserId((String) invocation.getArguments()[0]);
+            MarkInfo mark = new MarkInfo();
+            mark.setCourseId(BigInteger.ONE);
+            mark.setQuestionId(BigInteger.ONE);
+
+            List<MarkInfo> marks = user.getMarks();
+            marks.add(mark);
+            user.setMarks(marks);
+
+            return user;
+        });
+
+        when(mockCourseRep.findById(any(BigInteger.class))).thenAnswer(invocation -> {
+            CourseInfo course = new CourseInfo();
+            course.setId((BigInteger) invocation.getArguments()[0]);
+            return course;
+        });
+
+        when(mockQuestionRep.findByCourseId(any(BigInteger.class))).thenAnswer(invocation -> {
+            List<QuestionInfo> qs = new ArrayList<QuestionInfo>();
+            for(int i = 0; i<10;++i){
+                QuestionInfo q = new QuestionInfo();
+                q.setId(BigInteger.valueOf(i));
+                q.setQuestion("test");
+                q.setAnswer("test");
+                q.setCourseId((BigInteger) invocation.getArguments()[0]);
+                qs.add(q);
+            }
+
+            return qs;
+
+        });
+
+
+        when(mockUserRep.save(any(UserInfo.class))).then(returnsFirstArg());
 
     }
 
-    @Test
-    public void getById() throws Exception {
-
-    }
-
-    @Test
-    public void readAll() throws Exception {
-
-    }
-
-    @Test
-    public void readAll1() throws Exception {
-
-    }
 
     @Test
     public void update() throws Exception {
-
+        UserInfo user = new UserInfo();
+        user.setUserId("Login");
+        dao = new UserDaoImpl(mockUserRep,mockMarkrep,mockCourseRep,mockQuestionRep);
+        user = dao.update(user);
+        assertEquals("Login",user.getUserId());
     }
 
     @Test
-    public void delete() throws Exception {
+    public void subscribeSucces() throws Exception {
+        UserInfo user = new UserInfo();
+        List<BigInteger> courses = user.getSubscribedCourses();
+        courses.add(BigInteger.ONE);
 
+        dao = new UserDaoImpl(mockUserRep,mockMarkrep,mockCourseRep,mockQuestionRep);
+
+        Assert.assertTrue(dao.subscribe(user,BigInteger.TEN));
+        Assert.assertFalse(dao.subscribe(user,BigInteger.ONE));
     }
 
     @Test
-    public void idExists() throws Exception {
+    public void unsubscribeSuccess() throws Exception {
+        UserInfo user = new UserInfo();
+        List<BigInteger> courses = user.getSubscribedCourses();
+        courses.add(BigInteger.ONE);
 
-    }
+        dao = new UserDaoImpl(mockUserRep,mockMarkrep,mockCourseRep,mockQuestionRep);
 
-    @Test
-    public void subscribe() throws Exception {
-
-    }
-
-    @Test
-    public void unsubscribe() throws Exception {
-
+        Assert.assertTrue(dao.unsubscribe(user,BigInteger.ONE));
+        Assert.assertFalse(dao.unsubscribe(user,BigInteger.TEN));
     }
 
 }
