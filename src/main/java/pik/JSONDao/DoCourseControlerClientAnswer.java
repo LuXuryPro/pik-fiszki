9bef648181551563c476936fc319f87508e33e54
Radek <radek1236987@gmail.com>
2016-06-08 00:36:38 +0200

    Add processRequest implementation

--- src/main/java/pik/JSONDao/DoCourseControlerClientAnswer.java
+++ src/main/java/pik/JSONDao/DoCourseControlerClientAnswer.java
@@ -1,4 +1,20 @@
 package pik.JSONDao;
 
 public class DoCourseControlerClientAnswer {
+    public DoCourseControlerClientAnswer(int mark) {
+        this.mark = mark;
+    }
+
+    public int getMark() {
+
+        return mark;
+    }
+
+    public void setMark(int mark) {
+        this.mark = mark;
+    }
+
+    int mark;
+    int courseId;
+    int questionId;
 }
--- src/main/java/pik/JSONDao/DoCourseControllerClientFicheRequest.java
+++ src/main/java/pik/JSONDao/DoCourseControllerClientFicheRequest.java
@@ -1,4 +1,30 @@
 package pik.JSONDao;
 
+import java.math.BigInteger;
+
 public class DoCourseControllerClientFicheRequest {
+    BigInteger courseId;
+
+    public DoCourseControllerClientFicheRequest(BigInteger courseId, String userId) {
+        this.courseId = courseId;
+        this.userId = userId;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public BigInteger getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(BigInteger courseId) {
+        this.courseId = courseId;
+    }
+
+    String userId;
 }
