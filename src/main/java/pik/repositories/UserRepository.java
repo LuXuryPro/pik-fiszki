f28f853eeef41a0b10af58175feb1455ccf8372a
Michał Kamiński <M.J.Kaminski@stud.elka.pw.edu.pl>
2016-06-08 02:57:37 +0200

    Kometarze

--- src/main/java/pik/repositories/UserRepository.java
+++ src/main/java/pik/repositories/UserRepository.java
@@ -7,10 +7,40 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.math.BigInteger;
 import java.util.List;
 
+/**
+ * Interfejs Dostępu do podstawowych operacji na kolekcji uzytkownikow
+ */
 public interface UserRepository extends MongoRepository <UserInfo,String> {
+    /**
+     * Find by first name list.
+     *
+     * @param firstName the first name
+     * @return the list
+     */
     List<UserInfo> findByFirstName(String firstName);
+
+    /**
+     * Find by last name list.
+     *
+     * @param lastName the last name
+     * @return the list
+     */
     List<UserInfo> findByLastName (String lastName);
+
+    /**
+     * Find by email user info.
+     *
+     * @param eMail the e mail
+     * @return the user info
+     */
     UserInfo findByEmail(String eMail);
+
+    /**
+     * Find by user id user info.
+     *
+     * @param id the id
+     * @return the user info
+     */
     UserInfo findByUserId(String id);
 
     /*@Query(value = "{ 'subscribedcourses' : {$all : [?0] }}")
