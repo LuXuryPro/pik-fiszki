31262fe91f583681776b6830c4c636cbe26940ae
Konrad <k.czarnota27@gmail.com>
2016-06-09 23:52:04 +0200

    Fix slashes

---
+++ src/main/java/pik/controllers/ShowCourseController.java
@@ -0,0 +1,57 @@
+package pik.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import pik.JSONDao.DoCourseControlerClientAnswer;
+import pik.JSONDao.DoCourseControlerFishe;
+import pik.JSONDao.DoCourseControllerClientFicheRequest;
+import pik.Util.FacebookHelper;
+import pik.dto.QuestionInfo;
+import pik.exceptions.CourseAccessException;
+
+import java.math.BigInteger;
+import java.security.Principal;
+import java.util.List;
+
+@Controller
+public class ShowCourseController
+{
+    private QuestionsController questionsController;
+    private CourseController courseController;
+
+    @Autowired
+    public ShowCourseController(QuestionsController questionsController, CourseController courseController) {
+        this.questionsController = questionsController;
+        this.courseController = courseController;
+    }
+
+    @RequestMapping("/show-course/{courseID}")
+    public String showCourseInfo(@PathVariable(value = "courseID") BigInteger courseId, Principal principal, Model model) {
+        FacebookHelper facebookHelper = new FacebookHelper(principal);
+        try {
+            List<QuestionInfo> questions = questionsController.getAllQuestions(facebookHelper.getId(), courseId);
+            model.addAttribute("questions", questions);
+        } catch (Exception e) {
+        }
+        String courseName = courseController.getCourseNameById(courseId);
+        model.addAttribute("userId", facebookHelper.getId());
+        model.addAttribute("courseId", courseId);
+        model.addAttribute("courseName", courseName);
+        return "coursePage";
+    }
+
+
+        @RequestMapping(value = "/add-question")
+        public String doQuestion(Model model, Principal principal, @RequestParam("id") BigInteger id ,
+            @RequestParam("question") String question, @RequestParam("answer") String answer)
+        {
+            FacebookHelper f = new FacebookHelper(principal);
+            try {
+                questionsController.addQuestion(answer, question, id, f.getId());
+            }catch(Exception e){}
+
+            return "redirect:/show-course/" + id;
+        }
+}
