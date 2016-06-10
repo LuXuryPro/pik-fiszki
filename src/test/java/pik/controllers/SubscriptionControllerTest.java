39b061d0810d886eaa3afc7112c492bf952a8b8a
Radek <radek@rzaluska.com>
2016-06-10 16:31:17 +0200

    Add tests

--- src/test/java/pik/controllers/SubscriptionControllerTest.java
+++ src/test/java/pik/controllers/SubscriptionControllerTest.java
@@ -3,13 +3,26 @@ package pik.controllers;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import pik.dao.UserDao;
 
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class SubscriptionControllerTest {
+
+    private UserController userController;
+    private CourseController courseController;
+    private MockMvc mockMvc;
+
     @Before
     public void setUp() throws Exception {
-
+        this.userController = Mockito.mock(UserController.class);
+        this.courseController = Mockito.mock(CourseController.class);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(new SubscriptionController(userController, courseController)).build();
     }
 
     @After
@@ -19,7 +32,8 @@ public class SubscriptionControllerTest {
 
     @Test
     public void addSubscription() throws Exception {
-
+        mockMvc.perform(get("/add-sub"))
+                .andExpect(status().isOk());
     }
 
     @Test
